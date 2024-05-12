f1240d39db9efbe06a701ba369f8a191df1ce6f3
Mustafa Berk <mustafaberk1996@gmail.com>
2024-05-12 23:44:45 +0300

    screen titles are implemented.

--- composeApp/src/commonMain/kotlin/ui/AddUser.kt
+++ composeApp/src/commonMain/kotlin/ui/AddUser.kt
@@ -1,11 +1,8 @@
 package ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -16,20 +13,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import data.dao.UserDao
 import data.entity.User
 
 
 @Composable
-fun Plus(userDao: UserDao) {
-    Text("Plus")
+fun AddUser(userDao: UserDao) {
+    Column(modifier = Modifier.padding(10.dp)) {
 
-    Column {
         var name by remember { mutableStateOf("") }
         var surname by remember { mutableStateOf("") }
         var clickedSave by remember { mutableStateOf(false) }
+
+
 
         if (clickedSave) {
             LaunchedEffect(Unit) {
@@ -39,6 +36,8 @@ fun Plus(userDao: UserDao) {
                 clickedSave = false
             }
         }
+
+
         TextField(
             value = name,
             placeholder = {
@@ -55,9 +54,13 @@ fun Plus(userDao: UserDao) {
             onValueChange = { surname = it },
             modifier = Modifier.fillMaxWidth()
         )
-        Button(onClick = {
+        Button(
+            onClick = {
             clickedSave = true
-        }) {
+        },
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
             Text("Save")
         }
 
